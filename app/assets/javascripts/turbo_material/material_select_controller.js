505b5f76ac537d82fe05c3ed8470e75bedcff27b
Sergey Moiseev <moiseev.sergey@gmail.com>
2024-04-04 22:33:10 +0300

    Outlined Material Select

--- app/assets/javascripts/turbo_material/material_select_controller.js
+++ app/assets/javascripts/turbo_material/material_select_controller.js
@@ -1,10 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    connect() {
-        const select = mdc.select.MDCSelect.attachTo(this.element)
-    }
+  connect() {
+    const select = mdc.select.MDCSelect.attachTo(this.element);
+    select.listen("MDCSelect:change", () => {
+      select.root.dispatchEvent(
+        new CustomEvent("submit-now", {
+          bubbles: true,
+          cancelable: true,
+        }),
+      );
+    });
+  }
 
-    disconnect() {
-    }
+  disconnect() {}
 }
