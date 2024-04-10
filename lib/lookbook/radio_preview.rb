b78203bf11eb4eac3c4de0409e640b634255a10f
Sergey Moiseev <moiseev.sergey@gmail.com>
2024-04-10 17:49:20 +0300

    Correct names in lookbook

--- lib/lookbook/radio_preview.rb
+++ lib/lookbook/radio_preview.rb
@@ -3,7 +3,7 @@ class RadioPreview < Lookbook::Preview
   # @param label text
   # @param disabled toggle
   # @param required toggle
-  def default(label: 'Input', disabled: false, required: false)
+  def default(label: 'Radio', disabled: false, required: false)
     render 'common/form', helper_name: 'material_radio', label: label, name: 'checkbox', id: 'Input', disabled: disabled, required: required
   end
 end
--- lib/lookbook/textarea_preview.rb
+++ lib/lookbook/textarea_preview.rb
@@ -3,7 +3,7 @@ class TextareaPreview < Lookbook::Preview
   # @param label text
   # @param disabled toggle
   # @param required toggle
-  def default(label: 'Text', disabled: false, required: false)
+  def default(label: 'Textarea', disabled: false, required: false)
     render 'common/form', helper_name: 'material_textarea', label: label, name: 'textarea', id: 'Text', disabled: disabled, required: required
   end
 end
